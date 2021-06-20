115a2c616ecdbde814b450393976bfb802a9972a
Jan Varho <jan@varho.org>
2021-06-20 19:55:39 +0300

    Add default handler tests to core

---
+++ packages/libs/core/tests/handle/default-i18n.test.ts
@@ -0,0 +1,321 @@
+import { PrerenderManifest } from "next/dist/build";
+import {
+  Event,
+  handleDefault,
+  PageManifest,
+  prepareBuildManifests,
+  RoutesManifest
+} from "../../src";
+
+const event = (url: string): Event => {
+  return {
+    req: {
+      headers: [],
+      url
+    } as any,
+    res: {
+      end: jest.fn(),
+      setHeader: jest.fn()
+    } as any,
+    responsePromise: new Promise(() => ({}))
+  };
+};
+
+describe("Default handler (i18n)", () => {
+  let pagesManifest: { [key: string]: string };
+  let manifest: PageManifest;
+  let prerenderManifest: PrerenderManifest;
+  let routesManifest: RoutesManifest;
+  let getPage: any;
+
+  beforeAll(async () => {
+    prerenderManifest = {
+      version: 3,
+      notFoundRoutes: [],
+      routes: {
+        "/en/ssg": {
+          initialRevalidateSeconds: false,
+          srcRoute: null,
+          dataRoute: "unused"
+        },
+        "/fr/ssg": {
+          initialRevalidateSeconds: false,
+          srcRoute: null,
+          dataRoute: "unused"
+        },
+        "/en/fallback/prerendered": {
+          initialRevalidateSeconds: false,
+          srcRoute: null,
+          dataRoute: "unused"
+        },
+        "/fr/fallback/prerendered": {
+          initialRevalidateSeconds: false,
+          srcRoute: null,
+          dataRoute: "unused"
+        }
+      },
+      dynamicRoutes: {
+        "/fallback/[slug]": {
+          routeRegex: "unused",
+          dataRoute: "unused",
+          dataRouteRegex: "unused",
+          fallback: "/fallback/[slug].html"
+        }
+      },
+      preview: {
+        previewModeId: "test-id",
+        previewModeEncryptionKey: "test-key",
+        previewModeSigningKey: "test-sig-key"
+      }
+    };
+    routesManifest = {
+      basePath: "",
+      headers: [],
+      redirects: [
+        {
+          source: "/:nextInternalLocale(en|fr)/redirect-simple",
+          destination: "/:nextInternalLocale/redirect-target",
+          statusCode: 307
+        },
+        {
+          source: "/:nextInternalLocale(en|fr)/redirect/:dynamic",
+          destination: "/:nextInternalLocale/redirect-target/:dynamic",
+          statusCode: 308
+        }
+      ],
+      rewrites: [],
+      i18n: {
+        defaultLocale: "en",
+        locales: ["en", "fr"],
+        localeDetection: true
+      }
+    };
+    pagesManifest = {
+      "/en": "pages/en.html",
+      "/fr": "pages/fr.html",
+      "/en/404": "pages/en/404.html",
+      "/fr/404": "pages/fr/404.html",
+      "/en/500": "pages/en/500.html",
+      "/fr/500": "pages/fr/500.html",
+      "/en/[root]": "pages/en/[root].html",
+      "/fr/[root]": "pages/fr/[root].html",
+      "/en/html/[page]": "pages/en/html/[page].html",
+      "/fr/html/[page]": "pages/fr/html/[page].html",
+      "/ssr": "pages/ssr.js",
+      "/ssr/[id]": "pages/ssr/[id].js",
+      "/ssg": "pages/ssg.js",
+      "/fallback/[slug]": "pages/fallback/[slug].js"
+    };
+    const buildId = "test-build-id";
+    const publicFiles = ["favicon.ico", "name with spaces.txt"];
+    const manifests = await prepareBuildManifests(
+      { buildId, domainRedirects: {} },
+      {},
+      routesManifest,
+      pagesManifest,
+      prerenderManifest,
+      publicFiles
+    );
+    manifest = manifests.pageManifest;
+  });
+
+  beforeEach(() => {
+    jest.spyOn(console, "error").mockReturnValueOnce();
+    getPage = jest.fn();
+  });
+
+  describe("Public file", () => {
+    it.each`
+      uri
+      ${"/favicon.ico"}
+      ${"/name%20with%20spaces.txt"}
+    `("Routes $uri to public file", async ({ uri }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isPublicFile).toBeTruthy();
+        expect(route.file).toEqual(uri);
+      }
+    });
+  });
+
+  describe("Non-dynamic", () => {
+    it.each`
+      uri                | file
+      ${"/"}             | ${"pages/en.html"}
+      ${"/ssg"}          | ${"pages/en/ssg.html"}
+      ${"/not/found"}    | ${"pages/en/404.html"}
+      ${"/en"}           | ${"pages/en.html"}
+      ${"/en/ssg"}       | ${"pages/en/ssg.html"}
+      ${"/en/not/found"} | ${"pages/en/404.html"}
+      ${"/fr"}           | ${"pages/fr.html"}
+      ${"/fr/ssg"}       | ${"pages/fr/ssg.html"}
+      ${"/fr/not/found"} | ${"pages/fr/404.html"}
+    `("Routes static page $uri to file $file", async ({ uri, file }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual(file);
+      }
+    });
+
+    it.each`
+      uri                                        | file
+      ${"/_next/data/test-build-id/ssg.json"}    | ${"/_next/data/test-build-id/en/ssg.json"}
+      ${"/_next/data/test-build-id/en/ssg.json"} | ${"/_next/data/test-build-id/en/ssg.json"}
+      ${"/_next/data/test-build-id/fr/ssg.json"} | ${"/_next/data/test-build-id/fr/ssg.json"}
+    `("Routes static data route $uri to file $file", async ({ uri, file }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual(file);
+      }
+    });
+
+    it.each`
+      uri                                     | page
+      ${"/ssr"}                               | ${"pages/ssr.js"}
+      ${"/_next/data/test-build-id/ssr.json"} | ${"pages/ssr.js"}
+    `("Routes SSR request $uri to page $page", async ({ uri, page }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(getPage).toHaveBeenCalledWith(page);
+
+      // mocked getPage throws an error in render, so error page returned
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual("pages/en/500.html");
+      }
+    });
+  });
+
+  describe("Dynamic", () => {
+    it.each`
+      uri                   | file
+      ${"/foo"}             | ${"pages/en/[root].html"}
+      ${"/html/bar"}        | ${"pages/en/html/[page].html"}
+      ${"/fallback/new"}    | ${"pages/en/fallback/new.html"}
+      ${"/en/foo"}          | ${"pages/en/[root].html"}
+      ${"/en/html/bar"}     | ${"pages/en/html/[page].html"}
+      ${"/en/fallback/new"} | ${"pages/en/fallback/new.html"}
+      ${"/fr/foo"}          | ${"pages/fr/[root].html"}
+      ${"/fr/html/bar"}     | ${"pages/fr/html/[page].html"}
+      ${"/fr/fallback/new"} | ${"pages/fr/fallback/new.html"}
+    `("Routes static page $uri to file $file", async ({ uri, file }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual(file);
+      }
+    });
+
+    it.each`
+      uri                                                 | file
+      ${"/_next/data/test-build-id/fallback/new.json"}    | ${"/_next/data/test-build-id/en/fallback/new.json"}
+      ${"/_next/data/test-build-id/en/fallback/new.json"} | ${"/_next/data/test-build-id/en/fallback/new.json"}
+      ${"/_next/data/test-build-id/fr/fallback/new.json"} | ${"/_next/data/test-build-id/fr/fallback/new.json"}
+    `("Routes static data route $uri to file $file", async ({ uri, file }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual(file);
+      }
+    });
+
+    it.each`
+      uri                                       | page
+      ${"/ssr/1"}                               | ${"pages/ssr/[id].js"}
+      ${"/_next/data/test-build-id/ssr/1.json"} | ${"pages/ssr/[id].js"}
+    `("Routes SSR request $uri to page $page", async ({ uri, page }) => {
+      const route = await handleDefault(
+        event(uri),
+        manifest,
+        prerenderManifest,
+        routesManifest,
+        getPage
+      );
+
+      expect(getPage).toHaveBeenCalledWith(page);
+
+      // mocked getPage throws an error in render, so error page returned
+      expect(route).toBeTruthy();
+      if (route) {
+        expect(route.isStatic).toBeTruthy();
+        expect(route.file).toEqual("pages/en/500.html");
+      }
+    });
+  });
+
+  describe("Redirect", () => {
+    it.each`
+      uri                   | code   | destination
+      ${"/ssg/"}            | ${308} | ${"/ssg"}
+      ${"/favicon.ico/"}    | ${308} | ${"/favicon.ico"}
+      ${"/redirect-simple"} | ${307} | ${"/en/redirect-target"}
+      ${"/redirect/test"}   | ${308} | ${"/en/redirect-target/test"}
+    `(
+      "Redirects $uri to $destination with code $code",
+      async ({ code, destination, uri }) => {
+        const e = event(uri);
+        const route = await handleDefault(
+          e,
+          manifest,
+          prerenderManifest,
+          routesManifest,
+          getPage
+        );
+
+        expect(route).toBeFalsy();
+        expect(e.res.statusCode).toEqual(code);
+        expect(e.res.setHeader).toHaveBeenCalledWith("Location", destination);
+        expect(e.res.end).toHaveBeenCalled();
+      }
+    );
+  });
+});
